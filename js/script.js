d54a4e3a3f4b55b0bca836255afb81c7f182351f
lexbert17 <lexbertlazo@gmail.com>
2023-11-16 13:42:13 +0100

    add:correzione esercizio

--- js/script.js
+++ js/script.js
@@ -3,15 +3,34 @@ const { createApp }  = Vue;
 createApp({
     data() {
         return{
-            thingsToDo:["fare i compiti"],
-            whatToDo:[]
-            
-        }
+            newTodo: "",
+            task: [
+                {Text: "fare i compiti" , done: false},
+                {Text: "fare la spesa" , done: true},
+                {Text: "fare il bucato", done: false},
+            ],
+            errorVisible: false,           
+        };
     },
     methods: {
-        add: function() {
-            this.whatToDo.push(thingsToDo);        
+        addTodo() {
+            if (this.newTodo !== "") {
+                this.task.unshift ({
+                    text: this.newTodo,
+                    done: false,
+                });
+                this.newTodo = "";
+                
+            }else{
+                this.errorVisible = true;
+            }
+        },
+        toggleDone(clickIndex) {
+            this.task[clickIndex].done = !this.task[clickIndex].done;
+        },
+        deleteTodo (clickIndex) {
+            this.task.splice(clickIndex, 1);
         }
       
-    }
+    },
 }).mount(" #app")
